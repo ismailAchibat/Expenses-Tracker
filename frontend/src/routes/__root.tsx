9228b40d0f6146f514f189a5f2d9b63773682b50
ismailAchibat <ismailachibat4@gmail.com>
2024-06-27 14:41:06 +0100

    navbar for mobile devices

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -5,7 +5,20 @@ import {
   Outlet,
 } from "@tanstack/react-router";
 import { Toaster } from "../components/ui/sonner";
-
+import { cn } from "../lib/utils";
+import { Button } from "../components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "../components/ui/drawer";
+import { useState } from "react";
+import { CiMenuFries } from "react-icons/ci";
 
 export interface MyRouterContext {
   queryClient: QueryClient;
@@ -16,10 +29,10 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 });
 
 function NavBar() {
-
+  const [open, setOpen] = useState(false);
   return (
     <div className="p-2 flex justify-between mx-10">
-      <Link to='/' className="text-xl font-semibold">
+      <Link to="/" className="text-xl font-semibold">
         <span className="text-green-500">Expenses</span> Tracker
       </Link>
       <div className="md:flex gap-4 max-w-2xl hidden">
@@ -50,6 +63,56 @@ function NavBar() {
         >
           Profile
         </Link>
+      </div>
+      <div className="md:hidden flex items-center text-white">
+        <Drawer open={open} onOpenChange={setOpen}>
+          <DrawerTrigger asChild>
+            <CiMenuFries />
+          </DrawerTrigger>
+          <DrawerContent>
+            <DrawerHeader className="text-center">
+              <DrawerDescription>
+                <div className="flex flex-col text-lg">
+                  <Link
+                    to="/"
+                    className="[&.active]:font-bold [&.active]:text-green-500"
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    to="/about"
+                    className="[&.active]:font-bold [&.active]:text-green-500"
+                  >
+                    About
+                  </Link>
+                  <Link
+                    to="/expenses"
+                    className="[&.active]:font-bold [&.active]:text-green-500"
+                  >
+                    Expenses
+                  </Link>
+                  <Link
+                    to="/create_expense"
+                    className="[&.active]:font-bold [&.active]:text-green-500"
+                  >
+                    Create Expense
+                  </Link>
+                  <Link
+                    to="/profile"
+                    className="[&.active]:font-bold [&.active]:text-green-500"
+                  >
+                    Profile
+                  </Link>
+                </div>
+              </DrawerDescription>
+            </DrawerHeader>
+            <DrawerFooter className="pt-2">
+              <DrawerClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
       </div>
       <Toaster />
     </div>
